18eaabf4f2abcea63262699abd9f2f2e2d8dc842
Santiago <santialegre16@gmail.com>
2023-04-18 16:18:53 -0300

    Nuevo commit

--- script.js
+++ script.js
@@ -80,3 +80,9 @@ function scrollFunction() {
   }
 }
 
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', () => {
+  menu.classList.toggle('show');
+});
